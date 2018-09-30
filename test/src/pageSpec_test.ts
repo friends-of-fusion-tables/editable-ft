1d3dbca04a7c907dc7582c8b6cf6c25729ce9d84
Anno Langen <anno.langen@gmail.com>
2018-09-30 13:23:56 -0700

    test more

--- test/src/pageSpec_test.ts
+++ test/src/pageSpec_test.ts
@@ -1,5 +1,10 @@
-import {hash, parsePageSpec} from "../../js/pageSpec.js";
+import {hash, PageSpec, parsePageSpec} from '../../js/pageSpec.js';
 
-describe('PageSpec', () => it('parses its own hash', () => {
-  parsePageSpec(hash({tableId: 'T'})).should.deep.equal({tableId: 'T'});
-}));
+describe('PageSpec', () => {
+  it('parses its own hash',
+     () => [{tableId: 'T'}, {tableId: 'T', limit: 10}, {tableId: 'T', orderBy: 'foo'}, {
+             tableId: 'T',
+             filter: {foo: ['x', 'y']}
+           }].map((p: PageSpec) => parsePageSpec(hash(p)).should.deep.equal(p)));
+  it('falls back to empty if not a table', () => parsePageSpec('#blubber').should.deep.equal({}));
+});
